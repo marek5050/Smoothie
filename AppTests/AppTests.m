dd649575db2b63dd95e620f20c9a9650d959e816
marek5050 <marek.bejda@gmail.com>
2014-09-24 12:02:39 -0500

    Initial Commit of Project

---
+++ AppTests/AppTests.m
@@ -0,0 +1,39 @@
+//
+//  AppTests.m
+//  AppTests
+//
+//  Created by Marek Bejda on 9/24/14.
+//  Copyright (c) 2014 Smoothie. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AppTests : XCTestCase
+
+@end
+
+@implementation AppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

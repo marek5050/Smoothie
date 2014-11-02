7633e36d7b4ca0a7ec207d231710c8ecb6cff2db
SageMoore <sagemoore@utexas.edu>
2014-11-02 16:09:14 -0600

    added some color scheme code

---
+++ App/ColorScheme.h
@@ -0,0 +1,21 @@
+//
+//  ColorScheme.h
+//  App
+//
+//  Created by Megan Avery on 11/2/14.
+//  Copyright (c) 2014 Smoothie. All rights reserved.
+//
+
+#include <Foundation/Foundation.h>
+
+@interface ColorScheme : NSObject
+
+@property (nonatomic, strong) NSArray *options;
+@property (nonatomic, strong) NSMutableDictionary *forest;
+@property (nonatomic, strong) NSMutableDictionary *sunrise;
+@property (nonatomic, strong) NSMutableDictionary *midnight;
+
+@end
+
+
+
---
+++ App/ColorScheme.m
@@ -0,0 +1,20 @@
+//
+//  ColorScheme.m
+//  App
+//
+//  Created by Megan Avery on 11/2/14.
+//  Copyright (c) 2014 Smoothie. All rights reserved.
+//
+
+#import "ColorScheme.h"
+
+@implementation ColorScheme
+
+-(id) init {
+    self.options = @[@"Forest", @"Sunrise", @"Midnight"];
+    
+    return self;
+}
+
+
+@end

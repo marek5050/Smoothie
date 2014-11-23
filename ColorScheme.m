a7f37048537f6c26e3fab43ad97e805690d376fa
Megan Smith <meg1022@hinq.cs.utexas.edu>
2014-11-23 12:38:43 -0600

    some updates for the color scheme

--- ColorScheme.m
+++ ColorScheme.m
@@ -28,18 +28,32 @@
     [self.forest setObject:[UIColor greenColor]  forKey:@"backgroundColor"];
     [self.forest setObject:@"forest"  forKey:@"name"];
     [self.forest setObject:[UIColor blackColor]  forKey:@"textColor"];
+    
+    NSArray *colors = @[[UIColor brownColor], [UIColor orangeColor], [UIColor lightGrayColor], [UIColor darkGrayColor], [UIColor blackColor]];
+    
+    [self.forest setObject:colors forKey:@"colorsArray"];
 }
 
 -(void) setSunrise {
     [self.sunrise setObject:[UIColor orangeColor]  forKey:@"backgroundColor"];
     [self.sunrise setObject:@"sunrise" forKey:@"name"];
     [self.sunrise setObject:[UIColor blackColor]  forKey:@"textColor"];
+    
+    NSArray *colors = @[[UIColor redColor], [UIColor magentaColor], [UIColor purpleColor], [UIColor yellowColor], [UIColor cyanColor]];
+    
+    [self.sunrise setObject:colors forKey:@"colorsArray"];
+    
+    
 }
 
 -(void) setMidnight {
     [self.midnight setObject:[UIColor blackColor]  forKey:@"backgroundColor"];
     [self.midnight setObject:@"midnight"  forKey:@"name"];
     [self.midnight setObject:[UIColor whiteColor]  forKey:@"textColor"];
+    
+    NSArray *colors = @[[UIColor cyanColor], [UIColor greenColor], [UIColor blueColor], [UIColor redColor], [UIColor orangeColor]];
+    
+    [self.midnight setObject:colors forKey:@"colorsArray"];
 }
 
 @end

47691650df21608b2e65e08f587e19a878a4edff
Megan Smith <meg1022@hinq.cs.utexas.edu>
2014-11-23 15:27:20 -0600

    finished FAQ answers

--- App/HelpViewController.m
+++ App/HelpViewController.m
@@ -25,12 +25,12 @@
     
     self.questions = @[@"What is a property?", @"What is an account?", @"What is a profile", @"How do I add a property to an account?",  @"After I add a property to an account, how do I add Google Analytics to my website?", @"How do I add a profile to a property?"];
     
-    NSString *propertyExplanation = @"Each property corresponds to a website that you want to track with GA. An account can have multiple properties, aka websites it wants to keep track of";
+    NSString *propertyExplanation = @"Each property corresponds to a website that you want to track with GA. An account can have multiple properties, aka websites it wants to keep track of.";
     NSString *accountExplanation = @"An account is just a way to divide up the websites you want to keep track of. Each account can have multiple properties.";
-    NSString *profileExplanation = @"A profiles is a way to track specific things about a property. Each property must have at least one profile in order for the analytics to work";
-    NSString *addingProperty = @"FILL LATER";
-    NSString *addingGAToWebsite = @"FILL LATER";
-    NSString *addingProfile = @"FILL LATER";
+    NSString *profileExplanation = @"A profiles is a way to track specific things about a property. Each property must have at least one profile in order for the analytics to work.";
+    NSString *addingProperty = @"You can add a property by hitting the '+' in the top right corner of the home page. To create a property you just need to know 2 things: what you want to name the property and the URL of the website you want to track";
+    NSString *addingGAToWebsite = @"Once you have successfully created a property you will have the option to email yourself the corresponding javascript. You should email this javascript to yourself and then copy and paste it into the html for the website you want to track. Unfortunately this means you will only be able to track websites  you are the admin for.";
+    NSString *addingProfile = @"The homepage will show you a list of properties you have created for a specific account. You can add a profile to any property by hitting the 'Add Profile' cell in the list of profiles for that property. To add profile you only need to know what you want to name it. The nitty gritty of the profiles can be configured on the Google Analytics website.";
     
     self.answers = @[propertyExplanation, accountExplanation, profileExplanation, addingProperty, addingGAToWebsite, addingProfile];
     
